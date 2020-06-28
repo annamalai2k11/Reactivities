eecd1df04c103c505cccb0fbea7faab229cc4b0a
Annamalai <annamalai2k11@gmail.com>
2020-06-28 20:11:13 +0530

    Initial Comnit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options): base(options)
+        {   
+        }
+        public DbSet<Value> Values { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Value>().HasData(
+                new Value { Id = 1, Name="Value101"},
+                new Value { Id = 2, Name="Value102"},
+                new Value { Id = 3, Name="Value103"}
+            );
+        }
+    }
+}
